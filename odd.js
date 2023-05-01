400d2499a7b3af9f3acd4855d3eb808783642311
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-05-01 19:10:21 +0000

    this is a function for finding odd  number

---
+++ odd.js
@@ -0,0 +1,12 @@
+const isOdd = function(number){
+if (number %2 === 1){
+return number + " is odd";
+}
+else{
+return number + " is even";
+}
+}
+
+console.log(isOdd(17));
+console.log(isOdd(11));
+console.log(isOdd(16));
