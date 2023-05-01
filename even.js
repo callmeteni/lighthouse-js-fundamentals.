f163a7aba2c4ea3871105abcb12cfa9bb8c9e938
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-05-01 18:46:12 +0000

    this is a function for finding even number

---
+++ even.js
@@ -0,0 +1,12 @@
+const isEven = function(number){
+if (number %2 ===0){
+return number + " is Even";
+}
+else{
+return number + " is odd";
+}
+}
+
+console.log(isEven(16));
+console.log(isEven(11));
+console.log(isEven(16));
